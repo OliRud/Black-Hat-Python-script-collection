35f5072974ebd513d72c83c704f22a981951b14e
OliRd <160233200@student.rnngroup.ac.uk>
2024-08-19 20:24:34 +0100

    tcpclient complete

--- 1_TCP_Client.py
+++ 1_TCP_Client.py
@@ -1,1 +1,19 @@
-print("Hello World")
+import socket 
+
+target_host = "www.google.com"
+target_port = 80
+
+#create socket object
+client =  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#connect the client
+client.connect((target_host,target_port))
+
+#send data
+client.send(b"GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+
+#recieve data
+response = client.recv(4096)
+
+print("\nRESPONSE\n"+response.decode())
+client.close()
