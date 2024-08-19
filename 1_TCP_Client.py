5fc942ef27a2954c06dc4a9ca7487c00acda6363
OliRd <160233200@student.rnngroup.ac.uk>
2024-08-19 20:26:38 +0100

    added comments

--- 1_TCP_Client.py
+++ 1_TCP_Client.py
@@ -4,7 +4,7 @@ target_host = "www.google.com"
 target_port = 80
 
 #create socket object
-client =  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client =  socket.socket(socket.AF_INET, socket.SOCK_STREAM) #AF_NET indicates we're using IPv4 address and SOCK_STREAM to create a TCP client
 
 #connect the client
 client.connect((target_host,target_port))
@@ -17,3 +17,8 @@ response = client.recv(4096)
 
 print("\nRESPONSE\n"+response.decode())
 client.close()
+
+#this code makes the assumption that:
+#The connection will always go through
+#The server expects us to send data first
+#the server will always respond in a timely fashion
